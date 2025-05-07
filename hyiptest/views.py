e980845d42084886dc4ef96fcadc6effe6698aad
Roman Krotov <152505489+sishorian@users.noreply.github.com>
2025-05-07 15:42:57 +0300

    Rewrite QuestionViewSet to support TemplateHTML
    Rewrite QuestionViewSet from ModelViewSet to ViewSet,
    supporting TemplateHTMLRenderer.

--- hyiptest/views.py
+++ hyiptest/views.py
@@ -1,14 +1,40 @@
-from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
+from rest_framework import generics, mixins, renderers, viewsets
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from hyiptest.models import Question
 from hyiptest.serializers import QuestionSerializer
 
 
-'''
-# Old view that allowed to also render html templates.
-class QuestionList(mixins.ListModelMixin, generics.GenericAPIView):
+class QuestionList1(APIView):
     """
-    List all questions.
+    Old view. List all questions, with template support.
+
+    Can either return objects in API formats or in a regular template.
+    """
+
+    renderer_classes = [
+        renderers.TemplateHTMLRenderer,
+        renderers.JSONRenderer,
+        renderers.BrowsableAPIRenderer,
+    ]
+
+    def get(self, request, format=None):
+        queryset = Question.objects.all()
+        # TemplateHTMLRenderer takes a context dict, not requiring serialization.
+        if request.accepted_renderer.format == "html":
+            data = {"question_list": queryset}
+            return Response(data, template_name="hyiptest/question_list.html")
+        serializer = QuestionSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+class QuestionList2(mixins.ListModelMixin, generics.GenericAPIView):
+    """
+    Old view. List all questions, with template support.
+
+    Can either return objects in API formats or in a regular template.
     """
 
     queryset = Question.objects.all()
@@ -20,16 +46,45 @@ class QuestionList(mixins.ListModelMixin, generics.GenericAPIView):
     ]
 
     def get(self, request, *args, **kwargs):
-        # TemplateHTMLRenderer takes a context dict, not requiring serialization.
         if request.accepted_renderer.format == "html":
             # Get queryset with applied filtering; from self.list() code.
             queryset = self.filter_queryset(self.get_queryset())
             data = {"question_list": queryset}
             return Response(data, template_name="hyiptest/question_list.html")
         return self.list(request, *args, **kwargs)
-'''
 
 
-class QuestionViewSet(viewsets.ModelViewSet):
+class QuestionViewSet(viewsets.ViewSet):
+    """
+    List all questions or retrieve one, with template support.
+
+    Can either return objects in API formats or in a regular template.
+    """
+
+    renderer_classes = [
+        renderers.TemplateHTMLRenderer,
+        renderers.JSONRenderer,
+        renderers.BrowsableAPIRenderer,
+    ]
+
+    def list(self, request, format=None):
+        queryset = Question.objects.all()
+        if request.accepted_renderer.format == "html":
+            data = {"question_list": queryset}
+            return Response(data, template_name="hyiptest/question_list.html")
+        serializer = QuestionSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None, format=None):
+        queryset = Question.objects.all()
+        question = get_object_or_404(queryset, pk=pk)
+        if request.accepted_renderer.format == "html":
+            data = {"question": question}
+            return Response(data, template_name="hyiptest/question_detail.html")
+        serializer = QuestionSerializer(question)
+        return Response(serializer.data)
+
+
+class QuestionViewSet4(viewsets.ModelViewSet):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
