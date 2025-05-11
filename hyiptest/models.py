1e41087a358d3ea908514e333e01db09a2193164
Roman Krotov <152505489+sishorian@users.noreply.github.com>
2025-05-11 21:55:25 +0300

    Create BadSite and BadDomain models with tests
    See docstrings to learn what they do.

--- hyiptest/models.py
+++ hyiptest/models.py
@@ -1,6 +1,8 @@
 import uuid
 
+from django.core.validators import DomainNameValidator
 from django.db import models
+from django.db.models import Q
 from django.db.models.functions import Lower
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
@@ -25,12 +27,12 @@ class UUIDTimestampsModel(models.Model):
     created = models.DateTimeField(auto_now_add=True, help_text=_("Creation time"))
     updated = models.DateTimeField(auto_now=True, help_text=_("Modification time"))
 
+    def __str__(self):
+        return str(self.id)
+
     class Meta:
         abstract = True
         ordering = ["created"]  # will suffice initially for most models
-
-    def __str__(self):
-        return str(self.id)
 
 
 class Question(UUIDTimestampsModel):
@@ -47,19 +49,85 @@ class Question(UUIDTimestampsModel):
         help_text=_("Explain the question to the user"),
     )
 
-    class Meta(UUIDTimestampsModel.Meta):  # Django will set abstract=False.
-        constraints = [
-            models.UniqueConstraint(
-                Lower("text"),
-                name="question_text_unique_ci",  # 'ci' - case-insensitive
-                violation_error_message=_(
-                    "Same question text already exists (case-insensitive match)"
-                ),
-            ),
-        ]
-
     def __str__(self):
         return str(self.text)
 
     def get_absolute_url(self):
         return reverse("question-detail", kwargs={"pk": self.pk})
+
+    class Meta(UUIDTimestampsModel.Meta):  # Django will set abstract=False.
+        constraints = [
+            models.UniqueConstraint(
+                Lower("text"),
+                name="question_text_lower_unique",
+                violation_error_message=_("Question already exists (lowercase match)"),
+            ),
+        ]
+
+
+class BadSite(UUIDTimestampsModel):
+    """
+    Model representing a website known to be a fraud.
+    """
+
+    name = models.CharField(
+        max_length=50,
+        unique=True,
+        help_text=_("Name of the site or its company behind it"),
+    )
+
+    bad_type = models.CharField(
+        max_length=50,
+        help_text=_("What type of fraud it is, e.g. pyramid, scam, etc."),
+    )
+
+    def __str__(self):
+        return str(self.name)
+
+    class Meta(UUIDTimestampsModel.Meta):
+        constraints = [
+            models.UniqueConstraint(
+                Lower("name"),
+                name="badsite_name_lower_unique",
+                violation_error_message=_("BadSite already exists (lowercase match)"),
+            ),
+        ]
+
+
+class BadDomain(UUIDTimestampsModel):
+    """
+    Model representing one of the domains of a BadSite instance.
+    """
+
+    name = models.CharField(
+        max_length=100,
+        unique=True,
+        validators=[DomainNameValidator(message=_("Enter a valid domain name"))],
+        help_text=_("Domain name of the BadSite"),
+    )
+
+    site = models.ForeignKey(
+        BadSite,
+        on_delete=models.CASCADE,
+        related_name="domains",
+        help_text=_("Name of the site this domain belongs to"),
+    )
+
+    def __str__(self):
+        return str(self.name)
+
+    def clean(self):
+        """
+        Make sure domain names are lowercase.
+        """
+        self.name = self.name.lower()
+
+    class Meta(UUIDTimestampsModel.Meta):
+        constraints = [
+            # Check if it's lowercase in cases where clean() is not run.
+            # The UniqueConstraint is not needed.
+            models.CheckConstraint(
+                condition=Q(name=Lower("name")),
+                name="baddomain_name_is_lower",
+            ),
+        ]
--- hyiptest/tests/test_models.py
+++ hyiptest/tests/test_models.py
@@ -1,7 +1,8 @@
 from django import test
+from django.core.exceptions import ValidationError
 from django.db.utils import IntegrityError
 
-from hyiptest.models import Question
+from hyiptest.models import BadDomain, BadSite, Question
 
 
 class QuestionModelTest(test.TestCase):
@@ -54,7 +55,7 @@ class QuestionModelTest(test.TestCase):
         max_length = question._meta.get_field("text").max_length
         self.assertEqual(max_length, 100)
 
-    def test_text_unique_ci(self):
+    def test_text_lower_unique_constraint(self):
         """
         Ensure you can't create object with the same 'text' field.
 
@@ -80,3 +81,75 @@ class QuestionModelTest(test.TestCase):
         question = Question.objects.get()
         max_length = question._meta.get_field("description").max_length
         self.assertEqual(max_length, 200)
+
+
+# Don't do 'boring' tests for other models.
+
+
+class BadSiteModelTest(test.TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        BadSite.objects.create(
+            name="Test One",
+            bad_type="test",
+        )
+
+    def test_name_lower_unique_constraint(self):
+        """
+        Ensure you can't create object with the same 'name' field.
+
+        Even if the letters have different case.
+        """
+        with self.assertRaises(IntegrityError):
+            BadSite.objects.create(name="TesT oNe")
+
+
+class BadDomainModelTest(test.TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        badsite = BadSite.objects.create(
+            name="Test One",
+            bad_type="test",
+        )
+        BadDomain.objects.create(
+            name="testone.test",
+            site=badsite,
+        )
+
+    def setUp(self):
+        self.badsite = BadSite.objects.get(name="Test One")
+
+    def test_name_domain_validator(self):
+        """
+        Ensure that 'name' field accepts only domains.
+        """
+        baddomain = BadDomain(name="testone", site=self.badsite)
+        with self.assertRaises(ValidationError):
+            baddomain.full_clean()
+
+    def test_name_converted_lower(self):
+        """
+        Ensure that 'name' field is converted to lowercase on clean().
+        """
+        name = "tEStonE.cOm"
+        domain = BadDomain(name=name, site=self.badsite)
+        domain.full_clean()
+        self.assertEqual(domain.name, name.lower())
+
+    def test_name_lower_constraint(self):
+        """
+        Ensure you can't create object if 'name' field is not lowercase.
+
+        For cases where clean() is not run, e.g. when creating from shell.
+        """
+        with self.assertRaises(IntegrityError):
+            BadDomain.objects.create(name="testoNE.tESt")
+            # Btw, save() is not run here:
+            # BadDomain.objects.filter(name="testone.test").update(name="testoNE.tESt")
+
+    def test_site_related_name_domains(self):
+        """
+        Ensure the related object has correct backwards-relation Manager name.
+        """
+        field = self.badsite._meta.get_field("domains")
+        self.assertEqual(field.related_name, "domains")
