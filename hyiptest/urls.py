de6b77e75d5b31444c4825eae38f00961236562c
Roman Krotov <152505489+sishorian@users.noreply.github.com>
2025-11-15 23:52:25 +0300

    Add the test
    That asks user the questions.

--- hyiptest/urls.py
+++ hyiptest/urls.py
@@ -11,14 +11,20 @@ urlpatterns += [
 
 # Question
 urlpatterns += [
-    path("questions/", views.QuestionListView.as_view(), name="question-list"),
+    path(
+        "questions/",
+        views.QuestionListView.as_view(),
+        name="question-list",
+    ),
     path(
         "questions/<uuid:pk>/",
         views.QuestionDetailView.as_view(),
         name="question-detail",
     ),
     path(
-        "questions/create/", views.QuestionCreateView.as_view(), name="question-create"
+        "questions/create/",
+        views.QuestionCreateView.as_view(),
+        name="question-create",
     ),
     path(
         "questions/<uuid:pk>/update/",
@@ -34,7 +40,11 @@ urlpatterns += [
 
 # BadSite
 urlpatterns += [
-    path("badsites/", views.BadSiteListView.as_view(), name="badsite-list"),
+    path(
+        "badsites/",
+        views.BadSiteListView.as_view(),
+        name="badsite-list",
+    ),
     path(
         "badsites/<uuid:pk>/",
         views.BadSiteDetailView.as_view(),
@@ -45,4 +55,10 @@ urlpatterns += [
 # Check website domain
 urlpatterns += [
     path("search-domain/", views.search_domain, name="search-domain"),
+]
+
+# The test
+urlpatterns += [
+    path("test/", views.htest_question, name="htest-question"),
+    path("test/<uuid:progress_id>/", views.htest_question, name="htest-question"),
 ]
--- hyiptest/views.py
+++ hyiptest/views.py
@@ -1,10 +1,10 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.urls import reverse, reverse_lazy
 from django.views import generic
 
-from hyiptest.forms import SearchDomainForm
-from hyiptest.models import BadDomain, BadSite, Question
+from hyiptest.forms import SearchDomainForm, SelectAnswerForm
+from hyiptest.models import BadDomain, BadSite, Question, TestProgress
 
 
 class HomePageView(generic.TemplateView):
@@ -42,6 +42,53 @@ def search_domain(request):
         "found_badsite": found_badsite,
     }
     return render(request, "hyiptest/search_domain.html", context)
+
+
+# htest - short for hyiptest
+# `test_` can be confused with unittest.
+def htest_question(request, progress_id=None):
+    """
+    View function for asking user one question from the test.
+    """
+
+    if progress_id is None:  # user takes new test
+        current_question = (
+            Question.objects.order_by().first()  # start with the first created question
+        )
+        TestProgress.objects.all().delete()  # delete all previous saves
+        progress = TestProgress.objects.create(current_question=current_question)
+    else:
+        progress = TestProgress.objects.get(id=progress_id)  # skip handling wrong id
+        current_question = Question.objects.get(id=progress.current_question.id)
+
+    if request.method == "POST":
+        form = SelectAnswerForm(request.POST, question_obj=current_question)
+        if form.is_valid():
+            progress.risk_score += form.cleaned_data["selected_answer"].risk_score
+
+            # Find the questions after the current one, order them, get next
+            next_question = (
+                Question.objects.filter(created_at__gt=current_question.created_at)
+                .order_by("created_at")
+                .first()
+            )
+            progress.current_question = next_question
+            progress.save()
+            # Redirect to the test again, with updated progress
+            # TODO: Add code for the last question
+            return redirect("htest-question", progress_id=progress.id)
+    else:
+        form = SelectAnswerForm(question_obj=current_question)
+
+    context = {
+        "form": form,
+        "question": current_question,
+        "question_position": Question.objects.filter(
+            created_at__lte=current_question.created_at  # lt -> 0..(n-1), lte -> 1..n
+        ).count(),
+        "total_questions": Question.objects.count(),
+    }
+    return render(request, "hyiptest/htest_question.html", context)
 
 
 # Question
