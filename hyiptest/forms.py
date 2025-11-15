cffbc2641a14e53f2ca5e637c4eba34de20a302c
Roman Krotov <152505489+sishorian@users.noreply.github.com>
2025-11-15 21:58:44 +0300

    forms.py: create SelectAnswerForm
    For the test.

--- hyiptest/forms.py
+++ hyiptest/forms.py
@@ -11,6 +11,23 @@ def validate_lowercase(value):
         )
 
 
+class SelectAnswerForm(forms.Form):
+    """
+    Form to select one of the Answers of a Question.
+    """
+
+    # label is redundant
+    selected_answer = forms.ModelChoiceField(
+        queryset=None,
+        widget=forms.RadioSelect,
+    )
+
+    def __init__(self, *args, **kwargs):
+        question_obj = kwargs.pop("question_obj")
+        super().__init__(*args, **kwargs)
+        self.fields["selected_answer"].queryset = question_obj.answers.all()
+
+
 class SearchDomainForm(forms.Form):
     """
     Form for a user to search a website domain in the fraud database.
